62e4587e3ee0621ba7fa15c1758245cb7e28ecc7
Max <mkatr@softserveinc.com>
2022-12-07 03:01:59 +0200

    fixes

--- src/main/MyFirstClass.java
+++ src/main/MyFirstClass.java
@@ -3,6 +3,5 @@ package main;
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World! x2");
     }
 }
